7cb48d5e93c761eb4795ef77a73c45d327c95f88
John Weathers <weathers.johnb@gmail.com>
2023-07-26 09:34:30 -0500

    Add page imports, set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import {
   createRoutesFromElements, 
 } from 'react-router-dom';
 import Layout from './pages/Layout';
+import Index from './pages/Index';
+import Topic from './pages/Topic';
+import PostView from './pages/PostView';
+import PersistLogin from './pages/PersistLogin';
 import ErrorPage from './pages/ErrorPage';
 
 
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />} errorElement={<ErrorPage />}>
-
+    <Route element={<PersistLogin />}>
+      <Route path='/' element={<Layout />} errorElement={<ErrorPage />}>
+        <Route index element={<Index />}/>
+        <Route path='t/:topicName' element={<Topic />}/>
+        <Route path='t/:topicName/comments/:postId' element={<PostView />}/>
+      </Route>
     </Route>
+    
   )
 )
