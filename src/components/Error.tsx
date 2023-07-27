76155692a733175c9dc8285b9f053091252ff7a7
John Weathers <weathers.johnb@gmail.com>
2023-07-27 12:26:40 -0500

    Add conditional handling of auth required error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,18 +1,30 @@
-import { Dispatch, SetStateAction, RefObject } from 'react'
+import { useEffect, RefObject } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { SetErrMsg } from '../types';
+import Account from './Account';
 
 type ErrorProps = {
   errRef: RefObject<HTMLInputElement>,
-  errMsg: string,
-  setErrMsg: Dispatch<SetStateAction<string>>,
+  errMsg: string | string[],
+  setErrMsg: SetErrMsg,
 }
 
 const Error = ({ errRef, errMsg, setErrMsg }: ErrorProps) => {
+  useEffect(() => {
+    if (errMsg && errRef?.current) {
+      errRef.current.focus();
+    }
+  }, [errMsg])
+
+
   return (
     <div className={errMsg ? 'errmsg' : 'offscreen'}>
       <FontAwesomeIcon onClick={() => setErrMsg('')} icon={faCircleXmark} aria-label='close error message' className='x-close' size='xl' />
-      <p ref={errRef} aria-live='assertive' className='errmsg-p'>{errMsg}</p>
+      <p ref={errRef} aria-live='assertive' className='errmsg-p'>{errMsg?.[1] || errMsg}</p>
+      {errMsg[0] === 'auth required' && (
+        <Account popup={true} setErrMsg={setErrMsg} />
+      )}
     </div> 
   )
 }
