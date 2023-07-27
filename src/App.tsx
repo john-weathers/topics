63a16635b927486dfb9cc5d7731d5e0b20957c85
John Weathers <weathers.johnb@gmail.com>
2023-07-27 12:21:28 -0500

    Filling out routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import {
 } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Index from './pages/Index';
-import Topic from './pages/Topic';
+import TopicsOverview from './pages/TopicsOverview';
+import TopicFeed, { loader as topicFeedLoader } from './pages/TopicFeed';
 import PostView from './pages/PostView';
 import PersistLogin from './pages/PersistLogin';
 import ErrorPage from './pages/ErrorPage';
@@ -17,7 +18,8 @@ export const router = createBrowserRouter(
     <Route element={<PersistLogin />}>
       <Route path='/' element={<Layout />} errorElement={<ErrorPage />}>
         <Route index element={<Index />}/>
-        <Route path='t/:topicName' element={<Topic />}/>
+        <Route path='t/overview/status' element={<TopicsOverview />}/>
+        <Route path='t/:topicName' element={<TopicFeed />} loader={topicFeedLoader}/>
         <Route path='t/:topicName/comments/:postId' element={<PostView />}/>
       </Route>
     </Route>
