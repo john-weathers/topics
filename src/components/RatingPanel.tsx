b3dc68b4f7e42a7242f6713d4fbc3eb8d297795e
John Weathers <weathers.johnb@gmail.com>
2023-07-27 12:34:41 -0500

    Type narrowing for props

--- src/components/RatingPanel.tsx
+++ src/components/RatingPanel.tsx
@@ -2,14 +2,14 @@ import { FormEvent } from 'react'
 
 type RatingPanelProps = { 
   rating: number, 
-  handleVote: (e: FormEvent<HTMLFormElement>) => Promise<void>, 
-  voteStatus: string | undefined, 
-  voteType: string,
+  handleVote: (e: FormEvent<HTMLFormElement>) => Promise<void>, // update with return type
+  voteStatus: '+' | '-' | '', 
+  voteType: 'ratingvote' | 'relvote',
 }
 
 const RatingPanel = ({ rating, handleVote, voteStatus, voteType }: RatingPanelProps) => {
   return (
-    <div className={voteType === 'ratinvote' ? 'rating-panel' : 'rel-vote-panel'}>
+    <div className={voteType === 'ratingvote' ? 'rating-panel' : 'rel-vote-panel'}>
       <form onSubmit={handleVote}>
         <button name={voteType} value='+' style={{ backgroundColor: voteStatus === '+' ? 'red' : 'white' }}>+</button>
       </form>
