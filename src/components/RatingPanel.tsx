781568df40f0147314e577b38f205db0773abb7f
John Weathers <weathers.johnb@gmail.com>
2023-08-10 16:36:30 -0500

    Updates to accomodate use with relegation feature

--- src/components/RatingPanel.tsx
+++ src/components/RatingPanel.tsx
@@ -1,21 +1,22 @@
 import { FormEvent } from 'react'
 
 type RatingPanelProps = { 
-  rating: number, 
+  rating: number,
+  topic?: string, 
   handleVote: (e: FormEvent<HTMLFormElement>) => Promise<void>, // update with return type
   voteStatus: '+' | '-' | '', 
-  voteType: 'ratingvote' | 'relvote',
+  voteType: string,
 }
 
-const RatingPanel = ({ rating, handleVote, voteStatus, voteType }: RatingPanelProps) => {
+const RatingPanel = ({ rating, handleVote, voteStatus, voteType, topic }: RatingPanelProps) => {
   return (
     <div className={voteType === 'ratingvote' ? 'rating-panel' : 'rel-vote-panel'}>
       <form onSubmit={handleVote}>
-        <button name={voteType} value='+' style={{ backgroundColor: voteStatus === '+' ? 'red' : 'white' }}>+</button>
+        <button name={voteType} value={voteType === 'relvoteoverview' ? `+,${topic}` : '+'} style={{ backgroundColor: voteStatus === '+' ? 'red' : 'white' }}>+</button>
       </form>
       {rating}
       <form onSubmit={handleVote}>
-        <button name={voteType} value='-' style={{ backgroundColor: voteStatus === '-' ? 'blue' : 'white' }}>-</button>
+        <button name={voteType} value={voteType === 'relvoteoverview' ? `-,${topic}` : '-'} style={{ backgroundColor: voteStatus === '-' ? 'blue' : 'white' }}>-</button>
       </form>
     </div>
   )
