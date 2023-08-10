61c85da18c86d44f6a53acfb20032a98a46d5529
John Weathers <weathers.johnb@gmail.com>
2023-08-10 16:39:24 -0500

    Various updates to effects, event handlers, JSX

--- src/pages/PostView.tsx
+++ src/pages/PostView.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState, useEffect } from 'react';
+import { FormEvent, useState, useEffect, useRef } from 'react';
 import { useLocation, useOutletContext, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +6,10 @@ import { PostProps, SetErrMsg, CommentProps } from '../types';
 import Post from '../components/Post';
 import CommentForm from '../components/CommentForm';
 import Comment from '../components/Comment';
-import axios from '../api/axios';
+import useAuth from '../hooks/useAuth';
+import axios from 'axios';
+import axiosInstance from '../api/axios';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 const PostView = () => {
   // user will most likely navigate here through a feed
@@ -17,16 +20,19 @@ const PostView = () => {
   const [isLoading, setIsLoading] = useState(!post ? true : false);
   const { windowWidth, setErrMsg }: { windowWidth: number, setErrMsg: SetErrMsg } = useOutletContext();
   const params = useParams();
+  const { auth } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
   const [addComment, setAddComment] = useState(false);
   const [commentText, setCommentText] = useState('');
+  const commentRef = useRef<HTMLLIElement>(null);
 
   // load post data anyway (needed in case of direct URL navigation as well)
   // update post with fetched data
   useEffect(() => {
-    const URL = `/topics/${params.topicName}/comments/${params.postId}`;
+    const URL = `/topics/${params.topicName}/posts/${params.postId}`;
 
     const fetchPost = async () => {
-      const { data } = await axios.get<PostProps>(URL);
+      const { data } = await axiosInstance.get<PostProps>(URL);
       setPost((prev: PostProps | undefined) => {
         if (prev) {
           return {
@@ -43,10 +49,59 @@ const PostView = () => {
     fetchPost().catch((error) => {
       setErrMsg(error.message);
     })
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (post?.version && post.version !== 1) {
+      if (commentRef?.current) {
+        commentRef.current.scrollIntoView(false);
+      }
+    }
+  }, [post?.version])
 
   const handleNewComment = async (e: FormEvent) => {
     e.preventDefault();
+    
+    if (!commentText) {
+      setErrMsg('Text required to edit comment');
+      return;
+    }
+
+    if (commentText.length > 1000) {
+      setErrMsg('Comment too long');
+      return;
+    }
+
+    const URL = `/topics/${post.topicName}/posts/${post.id}/comments`
+    try {
+      const { data } = await axiosPrivate.post<CommentProps>(URL, {
+        text: commentText
+      })
+      data.ref = true;
+      setAddComment(false);
+      setPost((prev: PostProps) => {
+        return {
+          ...prev,
+          version: prev.version + 1,
+          comments: [...prev.comments.map((prevComment) => {
+            prevComment.ref = false;
+            return prevComment;
+          }), data]
+        }
+      })
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.response?.status === 409) {
+          setErrMsg('Username Taken');
+        } else if (err.response?.status === 400) {
+          setErrMsg('Complete all fields as instructed');
+        } else {
+          setErrMsg('Registration Failed');
+        }
+      } else {
+        setErrMsg('No Server Response');
+      }
+    }
 
   }
 
@@ -60,7 +115,13 @@ const PostView = () => {
     <div className='expanded-post-wrapper'>
       <Post post={post} setPost={setPost} windowWidth={windowWidth} wrapped={false} setErrMsg={setErrMsg}/>
       <div className='comments'>
-        <button type='button' onClick={() => setAddComment(true)}>+ Add Comment</button>
+        <button type='button' onClick={() => {
+          if (!auth?.accessToken) {
+            setErrMsg(['auth required', 'You must be logged in to comment']);
+            return;
+          }
+          setAddComment(true);
+        }}>+ Add Comment</button>
         {addComment && (
           <CommentForm 
             commentText={commentText}
@@ -72,11 +133,21 @@ const PostView = () => {
         )}
         {post.comments.length ? (
         <ul>
-          {post.comments.map((comment: CommentProps) => (
-            <li key={comment.id}>
-              <Comment postId={post.id} setPost={setPost} comment={comment} setErrMsg={setErrMsg} />
-            </li>
-          ))}
+          {post.comments.map((comment: CommentProps) => {
+            if (comment?.ref) {
+              return (
+                <li key={comment.id} ref={commentRef}>
+                  <Comment postId={post.id} topicName={post.topic.name} setPost={setPost} comment={comment} setErrMsg={setErrMsg} />
+                </li>
+              )
+            } else {
+              return (
+                <li key={comment.id}>
+                  <Comment postId={post.id} topicName={post.topic.name} setPost={setPost} comment={comment} setErrMsg={setErrMsg} />
+                </li>
+              )
+            }
+          })}
         </ul>
       ) : (
         <p>No comments! Be the first to add one!</p>
